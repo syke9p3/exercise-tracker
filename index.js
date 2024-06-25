dd59df8943fe5f2787b8eb5eb3c7527805b291b1
Kenth Saya-ang <gargarkenth93@gmail.com>
2024-06-25 15:56:08 +0000

    user model and route

--- index.js
+++ index.js
@@ -1,16 +1,57 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const { default: mongoose } = require('mongoose')
 require('dotenv').config()
 
 app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false, useUnifiedTopology: true }))
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected');
+});
 
+mongoose.connection.on('error', (err) => {
+  console.log('Mongoose default connection has occured ' + err + ' error');
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose default connection is disconnected');
+});
+
+const userSchema = new mongoose.Schema({
+  username: String
+})
+
+const User = mongoose.model('User', userSchema)
+
+app.post('/api/users', async (req, res) => {
+
+  console.log(req.body)
+
+  const newUser = new User({
+    username: req.body.username
+  })
+
+  try {
+    const savedNewUser = await newUser.save()
+    res.json({
+      username: savedNewUser.username,
+      _id: savedNewUser._id
+    })
+  } catch (err) {
+    res.json({ error: err })
+  }
+
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
