8aa2ff0ecd2f0cd116f7e86eb5245b610c3276a4
Kenth Saya-ang <gargarkenth93@gmail.com>
2024-06-29 14:41:57 +0000

    success

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ require('dotenv').config()
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false, useUnifiedTopology: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 mongoose.connection.on('connected', () => {
   console.log('Mongoose is connected');
@@ -27,15 +27,30 @@ mongoose.connection.on('disconnected', () => {
   console.log('Mongoose default connection is disconnected');
 });
 
+// MODEL
+
 const userSchema = new mongoose.Schema({
-  username: String
+  username: String,
+  exercises: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Exercise' }]
 })
 
 const User = mongoose.model('User', userSchema)
 
+
+const exerciseSchema = new mongoose.Schema({
+  username: String,
+  user_id: String,
+  description: String,
+  duration: Number,
+  date: String,
+})
+
+const Exercise = mongoose.model('Exercise', exerciseSchema)
+
+// ROUTES
+
 app.post('/api/users', async (req, res) => {
 
-  console.log(req.body)
 
   const newUser = new User({
     username: req.body.username
@@ -53,7 +68,137 @@ app.post('/api/users', async (req, res) => {
 
 })
 
+app.get('/api/users', async (req, res) => {
+
+  try {
+    const users = await User.find();
+    res.json(users)
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+
+const convertYYYYMMDDToDateString = (date) => {
+  const [year, month, day] = date.split('-');
+  return new Date(year, month - 1, day).toDateString();
+}
+
+
+app.post('/api/users/:_id/exercises', async (req, res) => {
+
+  const user = await User.findById(req.params._id)
+
+  if (!user) {
+    res.status(404).json({ error: 'User not found' })
+    return
+  }
+
+
+  const newExercise = new Exercise({
+    username: user.username,
+    user_id: user._id,
+    description: req.body.description,
+    duration: req.body.duration,
+    date: req.body.date ? convertYYYYMMDDToDateString(req.body.date) : new Date().toDateString()
+  })
+
+  const savedExercise = await newExercise.save()
+
+
+  if (!user.exercises) {
+    user.exercises = [];
+  }
+
+  user.exercises.push(savedExercise);
+  const newUser = await user.save();
+
+
+  res.json({
+    _id: savedExercise.user_id,
+    username: savedExercise.username,
+    date: savedExercise.date,
+    duration: savedExercise.duration,
+    description: savedExercise.description,
+  })
+
+})
+
+
+app.get('/api/users/:id', async (req, res) => {
+
+  try {
+    const user = await User.findById(req.params.id)
+    res.status(200).json({ username: user.username, _id: user._id })
+    console.log(user)
+
+  } catch (err) {
+    console.log(err)
+    res.status(404).json({ error: 'User not found' })
+
+  }
+
+})
+
+app.get("/api/users/:id/logs", async (req, res) => {
+  const { from, to, limit } = req.query;
+  const id = req.params.id;
+
+  const user = await User.findById(id).populate('exercises');
+  if (!user) {
+    res.json({ error: "User not found" });
+    return;
+  }
+
+  console.log("exercises by user ", user.username)
+  let exercises = user.exercises
+
+  if (from) {
+    const fromDate = new Date(from);
+    exercises = exercises.filter(exercise => new Date(exercise.date) >= fromDate);
+  }
+
+  if (to) {
+    const toDate = new Date(to);
+    exercises = exercises.filter(exercise => new Date(exercise.date) <= toDate);
+  }
+
+  if (limit) {
+    exercises = exercises.slice(0, limit);
+  }
+
+  const log = exercises.map(exercise => ({
+    description: exercise.description,
+    duration: exercise.duration,
+    date: exercise.date
+  }))
+
+  res.json({
+    _id: user._id,
+    username: user.username,
+    count: exercises.length,
+    log
+  });
+
+
+
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+
+async function getUser(userId) {
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return user;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
